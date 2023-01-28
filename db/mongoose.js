eae5c77deeb2d23be61e5738130e157156fd8957
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2023-01-28 05:26:33 -0500

    Try encoding the mongoose URI to avoid the MongoParseError: Incomplete key value pair for option

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,6 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/OutreachAPI', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(encodeURI(process.env.MONGODB_URI) || 'mongodb://localhost:27017/OutreachAPI', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 module.exports = { mongoose }
